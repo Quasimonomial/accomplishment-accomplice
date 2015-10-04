a7ab88eed7b24a88ac0dc934ce401fa18def219a
Brandon Contreras <heybrandon72@gmail.com>
2015-10-04 16:48:06 -0700

    application controller functionality added

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@
 #
 
 class User < ActiveRecord::Base
-  validates :email, :name, presence: true, uniqueness: true
+  validates :email, :name, presence: true
+  validates :email, uniqueness: true
 	validates :password, length: { minimum: 6, allow_nil: true }
 
 	attr_reader :password
@@ -21,7 +22,7 @@ class User < ActiveRecord::Base
 
 	after_initialize :ensure_session_token
 
-	def self.find_by_credentials(username, password)
+	def self.find_by_credentials(email, password)
 		user = User.find_by(username: username)
 		return nil unless user && user.valid_password?(password)
 		user
@@ -33,10 +34,10 @@ class User < ActiveRecord::Base
 	end
 
 	def valid_password?(password)
-		BCrypt::PAssword.new(self.password_digest).is_password?(password)
+		BCrypt::Password.new(self.password_digest).is_password?(password)
 	end
 
-	def reset_token!
+	def reset_session_token!
 		self.session_token = SecureRandom.urlsafe_base64(16)
 		self.save!
 		self.session_token
